e9ef825f8369fe8b220ecd18fa3c929ce841c59d
james hipe <jameszzz999888@gmail.com>
2020-01-15 17:32:36 +0800

    added some getter and setter

--- app/src/main/java/com/example/myapplication/User.java
+++ app/src/main/java/com/example/myapplication/User.java
@@ -1,6 +1,17 @@
 package com.example.myapplication;
 
 public class User {
-    private long empId;
-    private String firstname;
+    private long id;
+    private String email;
+
+
+    public User(){}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
