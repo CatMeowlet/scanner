fb7c09442e4e5c5cab0a028f2fbd21600f1439e8
james hipe <jameszzz999888@gmail.com>
2020-01-16 11:24:17 +0800

    fix all and want to add insert in mysql

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -15,7 +15,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     //View Objects
-    private Button btnRegisterEmail;
+    private Button btnRegisterEmail,btnMenu;
     private EditText editTextEmail;
     DatabaseHelper mDatabaseHelper;
     @Override
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnRegisterEmail = findViewById(R.id.btnRegisterEmail);
+        btnMenu = findViewById(R.id.btnMenu);
         editTextEmail = findViewById(R.id.editTextEmail);
         mDatabaseHelper = new DatabaseHelper(this);
 
@@ -32,18 +33,44 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View view) {
                     String newEntry = editTextEmail.getText().toString().trim();
-                    if(editTextEmail.length() != 0){
-                        AddData(newEntry);
-                        editTextEmail.setText("");
+                    if(mDatabaseHelper.count() == 0 ){
+                        //if there is no email registered yet
+                        if(editTextEmail.length() != 0){
+                            AddData(newEntry);
+                            editTextEmail.setText("");
+                        }else{
+                            toastMessage("Put Something in the field");
+                        }
                     }else{
-                        toastMessage("Put Something in the field");
+                        Toast.makeText(MainActivity.this,
+                                "You can only register once. please edit your email if you want to change", Toast.LENGTH_LONG).show();
+
+                        final Intent intent = new Intent(MainActivity.this , Menu.class);
+                        Thread thread = new Thread(){
+                            @Override
+                            public void run() {
+                                try {
+                                    Thread.sleep(3500); // As I am using LENGTH_LONG in Toast
+                                    startActivity(intent);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        };
+
+                        thread.start();
                     }
                 }
             });
         }else{
             startActivity(new Intent(this, Menu.class));
         }
-
+        btnMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this , Menu.class));
+            }
+        });
     }// end of on create
 
     public void AddData(String newEntry){
